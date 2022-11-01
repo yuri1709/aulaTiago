685829dbbd2b6ef98924fa4e61f64f29e688e245
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2022-11-01 08:57:58 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'phw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Photos-View';
+}
